915e8b96df2b378d1cdce9114b0083a4a54c51db
Abu.khater90 <abu.khater90@gmail.com>
2023-11-05 00:22:32 +0200

    update footer & contact us

--- src/components/5-Footer/Footer.jsx
+++ src/components/5-Footer/Footer.jsx
@@ -6,9 +6,19 @@ import './Footer.css';
 const Footer = () => {
   return (
     
-<footer>
+<footer className='flex'>
 
-footer
+<ul className='flex'>
+  <li><a href="">About</a></li>
+  <li><a href="">Projects</a></li>
+  <li><a href="">Speaking</a></li>
+  <li><a href="">Uses</a></li>
+</ul>
+
+
+<p>
+© 2023 Abu Khater , All Rights reserved.
+</p>
 
 </footer>
 
